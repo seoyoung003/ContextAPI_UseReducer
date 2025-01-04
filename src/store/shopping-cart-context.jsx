8902826eb1534a258bd08ef31921458dd8c6d647
seoyoungKim <seoyoungkim003@gmail.com>
2025-01-04 11:56:11 +0900

    finish

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext,  useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 import { DUMMY_PRODUCTS } from "../dummy-products";
 
@@ -9,66 +9,64 @@ export const CartContext = createContext({
 });
 
 function shoppingCartReducer(state, action) {
-
-  if(action.type === 'UPDATE_ITEM') {
+  if (action.type === "UPDATE_ITEM") {
     const updatedItems = [...state.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === action.payload.productId
-      );
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
 
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += action.payload.amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        ...state,
-        items: updatedItems,
-      };
-  }
-
-  if(action.type === 'ADD_ITEM') {
-  
-      const updatedItems = [...state.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === action.payload
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
-        updatedItems.push({
-          id: action.payload,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        ...state,
-        items: updatedItems,
-        
-      };
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
     };
-    
-  
-  return state;
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
   }
+
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
+  }
+
+  return state;
+}
 
 export default function CartContextProvider({ children }) {
   const [shoppingCartState, shoppingCartDispatch] = useReducer(
@@ -78,25 +76,21 @@ export default function CartContextProvider({ children }) {
     }
   );
 
-}
-
+  // 함수 정의가 분리되어야 합니다.
   function handleAddItemToCart(id) {
     shoppingCartDispatch({
-      type: 'ADD_ITEM',
-      payload: id
+      type: "ADD_ITEM",
+      payload: id,
     });
-
-   
+  }
 
   function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      shoppingCartDispatch({
-        type: 'UPDATE_ITEM',
-        payload: {
-          productId,
-          amount
-        }
-      })
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: {
+        productId,
+        amount,
+      },
     });
   }
 
